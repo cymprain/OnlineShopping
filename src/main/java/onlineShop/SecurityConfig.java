8941297751c4e0b6b7607f7121aa1743728dc965
cymprain <yingchen199@gmail.com>
2020-03-15 17:39:15 -0700

    Add frontend code and update Dao package.

--- src/main/java/onlineShop/SecurityConfig.java
+++ src/main/java/onlineShop/SecurityConfig.java
@@ -2,10 +2,12 @@ package onlineShop;
 
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -43,5 +45,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 	}
 	
+	@SuppressWarnings("deprecation")
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+	    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	}
+
 }
 
